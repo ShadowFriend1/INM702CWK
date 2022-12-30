463adf7f53d0484e1f3fabe159716774290c15f5
ShadowFriend1 <jack.negus3@gmail.com>
2022-12-30 14:19:01 +0000

    adjusted some variable names and added backwards propagation for relu and sigmoid layers

--- src/task_2/cnn.py
+++ src/task_2/cnn.py
@@ -1,9 +1,28 @@
-class my_cnn:
-    def __init__(self, data):
-        self.data = data
+import numpy as np
 
-    def sigmoid(self):
-        return self.data
 
-    def relu(self):
-        return self.data
+class My_Cnn:
+    def __init__(self, train_x, train_y, test_x, test_y):
+        self.train_x = train_x
+        self.train_y = train_y
+        self.test_x = test_x
+        self.test_y = test_y
+
+    @staticmethod
+    def sigmoid(z):
+        return 1 / (1 + np.exp(-z))
+
+    @staticmethod
+    def sigmoid_back(da, z):
+        sig = My_Cnn.sigmoid(z)
+        return da * sig * (1 - sig)
+
+    @staticmethod
+    def relu(z):
+        return np.maximum(0, z)
+
+    @staticmethod
+    def relu_back(da, z):
+        dz = np.array(da, copy=True)
+        dz[z <= 0] = 0
+        return dz
--- src/task_2/main.py
+++ src/task_2/main.py
@@ -1,14 +1,17 @@
 import struct
 
 import numpy as np
+from src.task_2.cnn import My_Cnn
+
 
 # Adapted from https://gist.github.com/tylerneylon/ce60e8a06e7506ac45788443f7269e40
-# Converts the MNIST .idx files to useable numpy arrays
+# Converts the MNIST .idx files to usable numpy arrays
 def read_idx(filename):
     with open(filename, 'rb') as f:
         zero, data_type, dims = struct.unpack('>HBB', f.read(4))
         shape = tuple(struct.unpack('>I', f.read(4))[0] for d in range(dims))
         return np.frombuffer(f.read(), dtype=np.uint8).reshape(shape)
+
 
 def load_mnist(location, tr_x_fn, tr_y_fn, te_x_fn, te_y_fn):
     train_x = read_idx(location + tr_x_fn)
@@ -22,3 +25,4 @@ if __name__ == "__main__":
     train_x, train_y, test_x, test_y = load_mnist("../../data/MNIST/", "train-images.idx3-ubyte",
                                                   "train-labels.idx1-ubyte", "t10k-images.idx3-ubyte",
                                                   "t10k-labels.idx1-ubyte")
+    cnn = My_Cnn(train_x, train_y, test_x, test_y)
