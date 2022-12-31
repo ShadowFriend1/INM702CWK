d7a1a3e9977332635f54af6e4d6526c6aa432aa7
ShadowFriend1 <jack.negus3@gmail.com>
2022-12-31 20:19:48 +0000

    cnn training implemented, tend towards predicting 0

--- src/task_2/cnn.py
+++ src/task_2/cnn.py
@@ -1,49 +1,162 @@
+import sys
+
 import numpy as np
 
 
+# A class representing the cnn
 class My_Cnn:
-    def __init__(self, train_x, train_y, test_x, test_y):
-        self.train_x = train_x
-        self.train_y = train_y
-        self.test_x = test_x
-        self.test_y = test_y
+    # Initialises the variables needed by the network
+    def __init__(self, layers, dropout_p, data_size, output_classes):
+        # layers is a list of what layers will make up the cnn along with how many nodes they will contain
+        self.layers = layers
+        # the dropout probability value to be applied in the dropout step
+        self.dropout_p = dropout_p
 
+        # Start with random weights for all layers
+        weights = []
+        for i in range(0, len(layers)):
+            if i > 0:
+                prev_layer = layers[i-1][1]
+            else:
+                prev_layer = data_size
+            if layers[i][0] in ("relu", "sigmoid"):
+                weights.append([np.random.randn(layers[i][1], prev_layer), np.random.randn(layers[i][1], 1)])
+            else:
+                print("No " + layers[i][0] + " Layer! During Initialisation")
+                sys.exit(1)
+
+        # Setup weights for the output layer
+        weights.append([np.random.randn(output_classes, output_classes), np.random.randn(output_classes, 1)])
+
+        self.weights = weights
+
+    # Returns the result of a sigmoid function applied to a value/array
     @staticmethod
     def sigmoid(z):
         return 1 / (1 + np.exp(-z))
 
+    # Return the result of an inverse sigmoid function applied to a value/array
     @staticmethod
-    def sigmoid_back(da, z):
-        sig = My_Cnn.sigmoid(z)
-        return da * sig * (1 - sig)
+    def sigmoid_back(z):
+        return z * (1 - z)
 
+    # Returns the results of a ReLU function applied to a value/array
     @staticmethod
     def relu(z):
         return np.maximum(0, z)
 
+    # Returns the results of an inverse ReLU function applied to a value/array
     @staticmethod
-    def relu_back(da, z):
-        dz = np.array(da, copy=True)
-        dz[z <= 0] = 0
-        return dz
+    def relu_back(z):
+        return z > 0
 
+    # Returns the results of a softmax function applied to a value/array
     @staticmethod
     def softmax(z):
-        return np.exp(z) / np.exp(z).sum()
+        return np.exp(z - np.max(z)) / np.exp(z - np.max(z)).sum()
+
+    # Applies dropout to an array, returning the array with dropout applied as well as the mask used
+    # (for backwards pass)
+    @staticmethod
+    def dropout(z, p):
+        mask = (np.random.uniform(low=0, high=1, size=z.shape) >= (1 - p)) / (1 - p)
+        return mask * z, mask
+
+    # Runs a forward pass through the cnn
+    def forward_pass(self, x):
+        z_layers = []
+        a_layers = []
+
+        # Pass through all layers
+        for i in range(0, len(self.layers)):
+            if i > 0:
+                prev_a = a_layers[i - 1]
+            else:
+                prev_a = x
+            z_layers.append(self.weights[i][0].dot(prev_a) + self.weights[i][1])
+            if self.layers[i][0] == "relu":
+                a_layers.append(My_Cnn.relu(z_layers[i]))
+            elif self.layers[i][0] == "sigmoid":
+                a_layers.append(My_Cnn.sigmoid(z_layers[i]))
+            else:
+                print("No " + self.layers[i][0] + " Layer! During Forward Pass")
+                sys.exit(1)
+
+        # Pass through softmax layer
+        z_layers.append(self.weights[-1][0].dot(a_layers[-1]) + self.weights[-1][1])
+        a_layers.append(My_Cnn.softmax(z_layers[-1]))
+
+        return z_layers, a_layers
+
+    # Takes in a result set and reorders it into an array of set size rows and n columns where each column represents a
+    # classification of the data (where 1 means true and 0 means false)
+    @staticmethod
+    def one_hot(y):
+        one_hot_y = np.zeros((y.size, y.max() + 1))
+        one_hot_y[np.arange(y.size), y] = 1
+        one_hot_y = one_hot_y.T
+        return one_hot_y
+
+    # Runs a backwards pass through the cnn
+    def backward_pass(self, x, y, z_layers, a_layers):
+        m = y.size
+        one_hot_y = My_Cnn.one_hot(y)
+        d_w_layers = []
+        d_b_layers = []
+
+        # Backward pass through sigmoid layer
+        dz_f = a_layers[-1] - one_hot_y
+        d_w_layers.append(1 / m * dz_f.dot(a_layers[-2].T))
+        d_b_layers.append(1 / m * np.sum(dz_f))
+
+        prev_dz = dz_f
+
+        # Backward pass through rest of layers
+        for i in range(len(self.layers) - 1, -1, -1):
+            if i > 0:
+                prev_a = a_layers[i - 1]
+            else:
+                prev_a = x
+            if self.layers[i][0] == "relu":
+                dz = self.weights[-1][0].T.dot(prev_dz) * My_Cnn.relu_back(z_layers[i])
+                prev_dz = dz
+            elif self.layers[i][0] == "sigmoid":
+                dz = self.weights[-1][0].T.dot(prev_dz) * My_Cnn.sigmoid_back(z_layers[i])
+                prev_dz = dz
+            else:
+                print("No " + self.layers[i][0] + " Layer! During Backward Pass")
+                sys.exit(1)
+            d_w_layers.append(1 / m * dz.dot(prev_a.T))
+            d_b_layers.append(1 / m * np.sum(dz))
+
+        d_w_layers.reverse()
+        d_b_layers.reverse()
+        return d_w_layers, d_b_layers
+
+    # Updates the network layer weights
+    def update_weights(self, d_w_layers, d_b_layers, alpha):
+        for i in range(0, len(self.weights)):
+            self.weights[i][0] -= alpha * d_w_layers[i]
+            self.weights[i][1] -= alpha * d_b_layers[i]
 
     @staticmethod
-    def dropout_mask(z, p):
-        mask = (np.random.uniform(low=0, high=1, size=z.shape) < p) / p
-        print(z.shape, mask.shape)
-        return mask
+    def get_predictions(a_f):
+        print(np.max(np.argmax(a_f, 0)))
+        return np.argmax(a_f, 0)
 
     @staticmethod
-    def dropout(z, mask):
-        print((mask * z).shape)
-        return mask * z
+    def get_accuracy(predictions, y):
+        return np.sum(predictions == y) / y.size
 
-    def forward_pass(self, layers):
-        return layers, self.train_x
+    def train(self, x, y, alpha, iterations):
+        for i in range(0, iterations):
+            z_layers, a_layers = self.forward_pass(x)
+            d_w_layers, d_b_layers = self.backward_pass(x, y, z_layers, a_layers)
+            self.update_weights(d_w_layers, d_b_layers, alpha)
+            if i % 50 == 0:
+                print("iteration: ", i)
+                predictions = My_Cnn.get_predictions(a_layers[-1])
+                print(My_Cnn.get_accuracy(predictions, y))
+        predictions = My_Cnn.get_predictions(a_layers[-1])
+        return My_Cnn.get_accuracy(predictions, y)
 
-    def backward_pass(self, layers):
-        return layers, self.train_x
--- src/task_2/main.py
+++ src/task_2/main.py
@@ -1,4 +1,5 @@
 import struct
+import sys
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -14,22 +15,34 @@ def read_idx(filename):
         return np.frombuffer(f.read(), dtype=np.uint8).reshape(shape)
 
 
-def load_mnist(location, tr_x_fn, tr_y_fn, te_x_fn, te_y_fn):
-    train_x = read_idx(location + tr_x_fn)
-    train_y = read_idx(location + tr_y_fn)
-    test_x = read_idx(location + te_x_fn)
-    test_y = read_idx(location + te_y_fn)
-    return train_x, train_y, test_x, test_y
+# loads MNIST dataset and shuffles the testing and training set according to a seed
+# converts the image data from 28x28 to 784x1
+def load_mnist(location, tr_x_fn, tr_y_fn, te_x_fn, te_y_fn, seed):
+    tr_x_raw = np.array(read_idx(location + tr_x_fn), copy=True)
+    tr_x = []
+    for n in range(0, len(tr_x_raw)):
+        tr_x.append(np.reshape(tr_x_raw[n], tr_x_raw[n].size))
+    tr_x = np.array(tr_x)
+    tr_x = tr_x.T
+    tr_y = read_idx(location + tr_y_fn)
+    te_x_raw = np.array(read_idx(location + te_x_fn), copy=True)
+    te_x = []
+    for n in range(0, len(te_x_raw)):
+        te_x.append(np.reshape(te_x_raw[n], te_x_raw[n].size))
+    te_x = np.array(te_x)
+    te_x = te_x.T
+    te_y = read_idx(location + te_y_fn)
+    print("Data Loaded!")
+    print("Train set size: " + str(len(tr_x[0])))
+    print("Test set size: " + str(len(te_x[0])))
+    return tr_x, tr_y, te_x, te_y
 
 
 if __name__ == "__main__":
+    random_seed = np.random.randint(0, 10000)
     train_x, train_y, test_x, test_y = load_mnist("../../data/MNIST/", "train-images.idx3-ubyte",
                                                   "train-labels.idx1-ubyte", "t10k-images.idx3-ubyte",
-                                                  "t10k-labels.idx1-ubyte")
-    cnn = My_Cnn(train_x, train_y, test_x, test_y)
-
-    plt.gray()
-    mask = cnn.dropout_mask(train_x[10], 0.5)
-    dropout = cnn.dropout(train_x[10], mask)
-    plt.imshow(dropout)
-    plt.show()
+                                                  "t10k-labels.idx1-ubyte", random_seed)
+    cnn = My_Cnn([["relu", 10]], 0.7, len(train_x), 10)
+    cnn.train(train_x, train_y, 0.1, 500)
+    sys.exit(0)
